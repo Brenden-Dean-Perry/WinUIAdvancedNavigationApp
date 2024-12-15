6490d4e1342efb11b3261a35864a74f5387d92c2
Brenden Perry <brendenperry5@gmail.com>
2024-12-14 19:49:18 -0800

    Update to app

---
+++ NavigationWinUI3Example/Classes/ConfigManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace NavigationWinUI3Example.Classes
+{
+    internal static class ConfigManager
+    {
+        internal static string GetAppSetting(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+
+        internal static string GetConnectionString(string key)
+        {
+            return ConfigurationManager.ConnectionStrings[key].ConnectionString;
+        }
+    }
+}
